a644ddc97ac4f696457fd79f8bf32872ed9c6ccc
Ines <ines@nucleartesuji.com>
2022-01-05 13:34:01 -0300

    Entrega primer sesion

---
+++ src/com/empresa/Main.java
@@ -0,0 +1,36 @@
+package com.empresa;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //numericos
+        byte variable1 = 5;
+        System.out.println(variable1);
+        short variable2= 10;
+        System.out.println(variable2);
+        int variable3 = 30;
+        System.out.println(variable3);
+        long variable4 = 100;
+        System.out.println(variable4);
+        float variable5 =5.5f;
+        System.out.println(variable5);
+        double variable6 =10.5d;
+        System.out.println(variable6);
+
+        //booleanos
+        boolean variable7= false;
+        System.out.println(variable7);
+        boolean variable8=true;
+        System.out.println(variable8);
+
+        //texto
+        char variable9='a';
+        System.out.println(variable9);
+        String variable10="Soy una arveja";
+        System.out.println(variable10);
+
+
+
+    }
+}
